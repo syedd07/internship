b91ec8942f90a328c77c5a4773627b3981d6e3a6
Syed Yousuf Ali <93760528+syedd07@users.noreply.github.com>
2024-07-12 00:52:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
-function verifyCertificate(certificateNumber) {
-    navigator.clipboard.writeText(certificateNumber).then(() => {
-        alert('Certificate number copied to clipboard. You will be redirected to Internshala for verification.');
-        window.open('https://internshala.com/verify_certificate', '_blank');
-    }, () => {
-        alert('Failed to copy the certificate number.');
+document.addEventListener('DOMContentLoaded', function() {
+    const buttons = document.querySelectorAll('.verify-button');
+
+    buttons.forEach(button => {
+        button.addEventListener('click', function() {
+            const certificateNumber = this.dataset.certificateNumber;
+            verifyCertificate(certificateNumber);
+        });
     });
-}
+
+    function verifyCertificate(certificateNumber) {
+        navigator.clipboard.writeText(certificateNumber).then(() => {
+            alert('Certificate number copied to clipboard. You will be redirected to Internshala for verification.');
+            window.open(`https://internshala.com/verify_certificate?certificate_number=${certificateNumber}`, '_blank');
+        }, () => {
+            alert('Failed to copy the certificate number.');
+        });
+    }
+});
