35ded7df27328e6918386d573b761adeb9495313
Syed Yousuf Ali <93760528+syedd07@users.noreply.github.com>
2024-07-12 21:54:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-}
 document.addEventListener('DOMContentLoaded', function() {
     const buttons = document.querySelectorAll('.verify-button');
 
@@ -11,14 +10,10 @@ document.addEventListener('DOMContentLoaded', function() {
 
     function verifyCertificate(certificateNumber) {
         navigator.clipboard.writeText(certificateNumber).then(() => {
-            // Success message
-            alert('Certificate number copied to clipboard. You will be redirected for verification.');
-            // Open verification URL in new tab
+            alert('Certificate number copied to clipboard. You will be redirected to Internshala for verification.');
             window.open(`https://internshala.com/verify_certificate?certificate_number=${certificateNumber}`, '_blank');
-        }).catch(err => {
-            // Error handling for clipboard access
-            console.error('Failed to copy certificate number:', err);
-            alert('Failed to copy the certificate number. Please try again.');
+        }, () => {
+            alert('Failed to copy the certificate number.');
         });
     }
 });
